0148ba9dce05a877b92bf631b26bc6d99d264be6
haniumme2000 <129924725+haniumme2000@users.noreply.github.com>
2024-04-20 16:53:54 +0530

    Update edit_me.sh

--- edit_me.sh
+++ edit_me.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
 # Replace this with the path to your email file
-EMAIL_FILE="[External] Test Scenario for 5G Call Flow.eml"
+#EMAIL_FILE="[External] Test Scenario for 5G Call Flow.eml"
 
 # Capture details using grep and awk
-subject=$(grep -i "^Subject:" "$EMAIL_FILE" | awk -F': ' '{print $4}')
-from=$(grep -i "^From:" "$EMAIL_FILE" | awk -F': ' '{print $2}')
-to=$(grep -i "^To:" "$EMAIL_FILE" | awk -F': ' '{print $2}')
-date=$(grep -i "^Date:" "$EMAIL_FILE" | awk -F': ' '{print $2}')
-Node_Name=$(grep -i "^NODE_NAME:" "$EMAIL_FILE" | awk -F': ' '{print $2}')
-Node_ID=$(grep -i "^NODE_ID:" "$EMAIL_FILE" | awk -F': ' '{print $2}')
+#subject=$(grep -i "^Subject:" "$EMAIL_FILE" | awk -F': ' '{print $4}')
+#from=$(grep -i "^From:" "$EMAIL_FILE" | awk -F': ' '{print $2}')
+#to=$(grep -i "^To:" "$EMAIL_FILE" | awk -F': ' '{print $2}')
+#date=$(grep -i "^Date:" "$EMAIL_FILE" | awk -F': ' '{print $2}')
+#Node_Name=$(grep -i "^NODE_NAME:" "$EMAIL_FILE" | awk -F': ' '{print $2}')
+#Node_ID=$(grep -i "^NODE_ID:" "$EMAIL_FILE" | awk -F': ' '{print $2}')
+
 
 # Print captured details
 echo "------------------------------------------"
 echo "------------------------------------------"
-echo "Subject: $subject"
-echo "From: $from"
-echo "To: $to"
-echo "Date: $date"
+echo "Subject: Test Scenario for 5G Call Flow"
+echo "From: User1"
+echo "To: User2"
+echo "Date: 20-04-1024"
 echo "------------------------------------------"
 echo "------------------------------------------"
 echo
-echo "Node_Name: $Node_Name"
-echo "Node_ID: $Node_ID"
+echo "Node_Name: SMSF, NEF"
+echo "Node_ID: SMSF123, NEF823"
 echo
 echo "------------------------------------------"
 echo "------------------------------------------"
